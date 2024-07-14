8159f7b6943bb916f20a27d0a99130e47e561025
Danilo Tardioli <dantard@unizar.es>
2024-07-14 15:34:21 +0200

    improved rclone_manager

--- src/swik/rclone_browser.py
+++ src/swik/rclone_browser.py
@@ -8,8 +8,41 @@ from PyQt5 import QtGui
 from PyQt5.QtCore import QTimer, pyqtSignal, QMimeData, Qt
 from PyQt5.QtGui import QDrag, QDragLeaveEvent
 from PyQt5.QtWidgets import QTreeWidget, QMainWindow, QWidget, QVBoxLayout, QApplication, QTreeWidgetItem, QMenu, QFileDialog, QComboBox, QLabel, QToolBar, \
-    QProgressBar, QAbstractItemView, QHBoxLayout
-from swik.progressing import Progressing
+    QProgressBar, QAbstractItemView, QHBoxLayout, QProgressDialog
+
+
+class Progressing(QProgressDialog):
+    done = pyqtSignal(object)
+
+    def __init__(self, parent, max_value=0, title=None, cancel=False):
+        super().__init__(parent)
+        self.setMaximum(max_value)
+        self.callback = None
+        self.setLabelText(title)
+        if not cancel:
+            self.setCancelButton(None)
+        self.show()
+
+    def set_progress(self, value):
+        self.setValue(int(value))
+        QApplication.processEvents()
+        return not self.wasCanceled()
+
+    def run(self):
+        self.ret_value = self.func(*self.args)
+        self.done.emit(self.ret_value)
+        if self.callback:
+            self.callback(self.ret_value, *self.args)
+        self.hide()
+
+    def get_return_value(self):
+        return self.ret_value
+
+    def start(self, func, *args, **kwargs):
+        self.func = func
+        self.args = args
+        self.callback = kwargs.get("callback", None)
+        QTimer.singleShot(50, self.run)
 
 
 class RCloneApi:
@@ -25,11 +58,12 @@ class RCloneApi:
         return self.run_rclone_command(command)
 
     def run_rclone_command(self, command):
-        """Runs an rclone command and returns the output."""
+        print("*** Running command: ", " ".join(command))
         result = subprocess.run(command, capture_output=True, text=True)
         if result.returncode != 0:
             print(f"Error: {result.stderr}")
             return None
+        print("done")
         return result.stdout
 
     def download(self, path, where):
@@ -41,12 +75,12 @@ class RCloneApi:
         return self.run_rclone_command(command)
 
     def copy2(self, src, dest):
-        self.src = str(src)
-        print("into copy", src, dest)
-        self.command = ["rclone", "copy", self.rclone_path + self.src, self.rclone_path + dest]
-        print(self.command, "kkk", " ".join(self.command))
-        self.run_rclone_command(self.command)
-        # os.system(" ".join(self.command))
+        command = ["rclone", "copy", self.rclone_path + src, self.rclone_path + dest]
+        self.run_rclone_command(command)
+
+    def delete(self, path):
+        command = ["rclone", "delete", self.rclone_path + path]
+        self.run_rclone_command(command)
 
 
 class RCloneTreeItem(QTreeWidgetItem):
@@ -92,27 +126,30 @@ class RCloneTreeItem(QTreeWidgetItem):
 
         self.setText(2, text_size)
 
+    def __lt__(self, other):
+        column = self.treeWidget().sortColumn()
+        if column == 1:
+            if self.is_dir and other.is_dir:
+                return self.text(0).lower() < other.text(0).lower()
+            elif self.is_dir and not other.is_dir:
+                return True
+            elif not self.is_dir and other.is_dir:
+                return False
+            elif self.text(1).lower() != other.text(1).lower():
+                return self.text(1).lower() < other.text(1).lower()
+            return self.text(0).lower() < other.text(0).lower()
 
-def __lt__(self, other):
-    column = self.treeWidget().sortColumn()
-    if column == 1:
-        if self.is_dir and not other.is_dir:
-            return True
-        if not self.is_dir and other.is_dir:
-            return False
-        return self.text(1).lower() < other.text(1).lower()
-    elif column == 2:
-        return self.size < other.size
-    return super().__lt__(other)
+        elif column == 2:
+            return self.size < other.size
+        return super().__lt__(other)
 
-
-def get_absolute_path(self):
-    path = self.path
-    parent = self.parent()
-    while parent:
-        path = parent.path + "/" + path
-        parent = parent.parent()
-    return path
+    def get_absolute_path(self):
+        path = self.path
+        parent = self.parent()
+        while parent:
+            path = parent.path + "/" + path
+            parent = parent.parent()
+        return path
 
 
 class LoadingItem(RCloneTreeItem):
@@ -121,8 +158,9 @@ class LoadingItem(RCloneTreeItem):
 
 
 class TreeWidget(QTreeWidget):
-    download_requested = pyqtSignal(str)
+    download_requested = pyqtSignal(object)
     move_requested = pyqtSignal(object, object)
+    delete_requested = pyqtSignal(object)
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -131,6 +169,7 @@ class TreeWidget(QTreeWidget):
         self.setDragEnabled(True)
         self.setAcceptDrops(True)
         self.setDragDropMode(QAbstractItemView.InternalMove)
+        self.setSelectionMode(QAbstractItemView.ExtendedSelection)
         self.aitem = None
 
     def set_interactable(self, value):
@@ -144,10 +183,7 @@ class TreeWidget(QTreeWidget):
         dest_item: RCloneTreeItem = self.itemFromIndex(index)
         source_items = self.selectedItems()
         if dest_item is not None and source_items is not None and dest_item.is_dir:
-            a0.acceptProposedAction()
             self.move_requested.emit(source_items, dest_item)
-        else:
-            a0.ignore()
 
     # super().dropEvent(a0)
 
@@ -176,17 +212,18 @@ class TreeWidget(QTreeWidget):
             index = self.indexAt(a0.pos())
             if index.isValid():
 
-                item = self.itemFromIndex(index)
-                path = item.path
-                while item.parent():
-                    path = item.parent().path + "/" + path
-                    item = item.parent()
+                # item = self.itemFromIndex(index)
 
                 menu = QMenu()
                 download = menu.addAction("Download")
+                delete = menu.addAction("Delete")
                 res = menu.exec_(self.viewport().mapToGlobal(a0.pos()))
+                selected = self.selectedItems()
+
                 if res == download:
-                    self.download_requested.emit(path)
+                    self.download_requested.emit(selected)
+                elif res == delete:
+                    self.delete_requested.emit(selected)
 
 
 class RCloneBrowser(QWidget):
@@ -194,7 +231,7 @@ class RCloneBrowser(QWidget):
         super().__init__()
 
         self.emit_enabled = True
-        self.api = RCloneApi("gdrive:")
+        self.api = RCloneApi("")
 
         # Set up the main window
         self.setWindowTitle("QTreeWidget Example")
@@ -203,51 +240,33 @@ class RCloneBrowser(QWidget):
         # Set up the central widget and layout
         layout = QVBoxLayout(self)
 
-        self.remotes_cb = QComboBox()
-        self.remotes_cb.currentIndexChanged.connect(self.on_remote_changed)
-
-        self.filter_cb = QComboBox()
-        self.filter_cb.addItems(["*", "PDF", "Image", "Video", "Audio", "Text", "Archive", "Other"])
-        self.filter_cb.currentIndexChanged.connect(self.on_filter_changed)
-        self.pb = QProgressBar()
-
-        self.protected = []
-
-        tb = QToolBar("Main")
-        tb.addWidget(QLabel("Remote: "))
-        self.protected.append(tb.addWidget(self.remotes_cb))
-        tb.addSeparator()
-
-        tb.addWidget(QLabel("Filter: "))
-        self.protected.append(tb.addWidget(self.filter_cb))
-
-        tb.addSeparator()
-        self.protected.append(tb.addAction("⟳", lambda: self.on_remote_changed(0)))
-        tb.addSeparator()
-        self.protected.append(tb.addAction("↓", lambda: self.download_btn_clicked(False)))
-        self.protected.append(tb.addAction("▶", lambda: self.download_btn_clicked(True)))
-
-        self.pc_act = tb.addWidget(self.pb)
-
         # Create a QTreeWidget
         self.tree = TreeWidget()
         self.tree.setHeaderLabels(["File", "Type", "Size"])
         self.tree.itemExpanded.connect(self.item_expanded)
         self.tree.itemCollapsed.connect(self.item_collapsed)
         self.tree.download_requested.connect(self.download_requested)
-        self.tree.move_requested.connect(self.move_requested)
+        self.tree.delete_requested.connect(self.delete_requested)
+        self.tree.move_requested.connect(self.copy_requested)
         self.tree.doubleClicked.connect(self.on_double_clicked)
         self.tree.setDragEnabled(True)
         self.tree.setDropIndicatorShown(True)
         # self.tree.setDragDropMode(QAbstractItemView.InternalMove)
 
-        layout.addWidget(tb)
         layout.addWidget(self.tree)
-        QTimer.singleShot(100, self.begin)
+        layout.setContentsMargins(0, 0, 0, 0)
 
-    def move_requested(self, source_items, dest_item):
-        for item in source_items:
-            self.copy(item, dest_item)
+        remotes = self.api.list_remotes().split("\n")
+        remotes = [remote for remote in remotes if remote != ""]
+
+        # self.api.set_remote(self.remotes_cb.currentText())
+        for remote in remotes:
+            rem = RCloneTreeItem(self.tree, {"Name": remote, "IsDir": True, "Path": remote})
+            LoadingItem(rem)
+        self.tree.sortByColumn(1, Qt.AscendingOrder)
+
+    def copy_requested(self, source_items, dest_item):
+        self.copy(source_items, dest_item)
 
     def set_accept_drops(self, value):
         self.tree.viewport().setAcceptDrops(value)
@@ -258,18 +277,17 @@ class RCloneBrowser(QWidget):
         remotes = [remote for remote in remotes if remote != ""]
         self.remotes_cb.addItems(remotes)
 
-    def on_remote_changed(self, index):
-        self.tree.clear()
-        self.api.set_remote(self.remotes_cb.currentText())
-        self.ls()
-
     def on_double_clicked(self, index):
         item: RCloneTreeItem = self.tree.itemFromIndex(index)
-        self.download(item.get_absolute_path(), "/tmp", True)
+        self.download([item], "/tmp", True)
 
-    def download_requested(self, path):
-        print("Download requested", path)
-        self.download(path)
+    def download_requested(self, items):
+        print("Download requested", items)
+        self.download(items)
+
+    def delete_requested(self, items):
+        print("Delete requested", items)
+        self.delete(items)
 
     def download_btn_clicked(self, run):
         indexes = self.tree.selectedIndexes()
@@ -282,64 +300,49 @@ class RCloneBrowser(QWidget):
     def on_filter_changed(self, index):
         self.on_remote_changed(0)
 
-    def pb_start(self, text=""):
-        self.pc_act.setVisible(True)
-        self.pb.setFormat(text)
-        self.pb.setMaximum(100)
-        self.pb.setValue(10)
-        # for i in self.protected:
-        #    i.setEnabled(False)
+    def copy(self, items, dest_item):
+        self.pd = Progressing(self, len(items), "Copying...", cancel=True)
 
-    def set_pb_percent(self, value):
-        self.pb.setValue(int(value))
+        def do_copy():
+            if dest_item.is_dir:
+                dest_dir = dest_item.get_absolute_path()
+            else:
+                dest_dir = os.path.dirname(dest_item.get_absolute_path())
 
-    def pb_finish(self):
-        self.pb.setValue(100)
-        self.pc_act.setVisible(False)
-        self.tree.set_interactable(True)
-        for i in self.protected:
-            i.setEnabled(True)
+            for source_item in items:
+                if self.pd.wasCanceled():
+                    break
+                self.pd.setLabelText("Copying " + source_item.get_absolute_path())
+                self.pd.set_progress(items.index(source_item))
+                QApplication.processEvents()
+                self.api.copy2(source_item.get_absolute_path(), dest_dir)
 
-    def copy(self, source_item, dest_item):
-        self.pb_start("Copying...")
-        if dest_item.is_dir:
-            dest_dir = dest_item.get_absolute_path()
-        else:
-            dest_dir = os.path.dirname(dest_item.get_absolute_path())
+            self.pd.setValue(len(items))
 
-        self.api.copy2(source_item.get_absolute_path(), dest_dir)
-        self.tree.blockSignals(True)
-        # self.emit_enabled = False
-        dest_item.takeChildren()
-        LoadingItem(dest_item)
-        # self.emit_enabled = True
-        self.tree.blockSignals(False)
+            self.tree.blockSignals(True)
+            dest_item.takeChildren()
+            LoadingItem(dest_item)
+            self.ls(dest_item.get_absolute_path(), dest_item)
+            self.tree.blockSignals(False)
 
-        self.ls(dest_item.get_absolute_path(), dest_item)
+        self.pd.start(do_copy)
 
     def ls(self, path=None, parent1=None):
-        self.pb_start("Loading...")
+        self.pd = Progressing(self, 0, "Listing " + path)
 
         def do_ls():
-            print("do ls", path)
-            res = self.api.ls(path)
-            try:
-                data = json.loads(res)
-            except:
-                return
 
-            data.sort(key=lambda x: not x["IsDir"])
+            res = self.api.ls(path)
+            data = json.loads(res)
+
+            if parent1 is not None:
+                self.remove_loading(parent1)
 
             for elem in data:
-                self.set_pb_percent(100 * data.index(elem) / len(data))
-
                 parent = RCloneTreeItem(parent1 if parent1 else self.tree, elem)
 
                 if parent.is_dir:
                     LoadingItem(parent)
-
-            if parent1 is not None:
-                parent1.takeChild(0)
 
             header = self.tree.header()
             if header:
@@ -348,9 +351,15 @@ class RCloneBrowser(QWidget):
                 self.tree.resizeColumnToContents(2)
 
             self.tree.sortByColumn(1, Qt.AscendingOrder)
-            self.pb_finish()
+            self.pd.close()
 
-        QTimer.singleShot(100, do_ls)
+        self.pd.start(do_ls)
+
+    def remove_loading(self, parent1):
+        for i in range(parent1.childCount()):
+            if isinstance(parent1.child(i), LoadingItem):
+                parent1.takeChild(i)
+                break
 
     def resizeEvent(self, a0: QtGui.QResizeEvent) -> None:
         header = self.tree.header()
@@ -359,31 +368,67 @@ class RCloneBrowser(QWidget):
             self.tree.resizeColumnToContents(1)
             self.tree.resizeColumnToContents(2)
 
-    def download(self, path, where=None, run=False):
+    def delete(self, items):
+        self.pd = Progressing(self, len(items), "Deleting", cancel=True)
+
+        def do_download():
+            parents = [item.parent() for item in items]
+            for item in items:
+                if self.pd.wasCanceled():
+                    break
+                self.pd.setValue(items.index(item))
+                path = item.get_absolute_path()
+                self.api.delete(path)
+
+            self.pd.setValue(len(items))
+
+            self.tree.blockSignals(True)
+            for parent in parents:
+                parent.takeChildren()
+                LoadingItem(parent)
+                self.ls(parent.get_absolute_path(), parent)
+            self.tree.blockSignals(False)
+
+        self.pd.start(do_download)
+
+    def download(self, items, where=None, run=False):
         if where is None:
-            where = QFileDialog.getExistingDirectory(None, "Save file", path.split("/")[-1])
+            where = QFileDialog.getExistingDirectory(None, "Save file", "", QFileDialog.ShowDirsOnly)
             if not where:
                 return
 
-        self.pb_start("Downloading...")
+        self.pd = Progressing(self, len(items), "Downloading", cancel=True)
 
         def do_download():
-            self.api.download(path, where)
-            self.pb_finish()
+            for item in items:
+                if self.pd.wasCanceled():
+                    break
+                self.pd.setLabelText("Downloading " + item.get_absolute_path())
+                self.pd.setValue(items.index(item))
+
+                path = item.get_absolute_path()
+                if item.is_dir:
+                    self.api.download(path, where + "/" + path.split("/")[-1])
+                else:
+                    self.api.download(path, where)
+
+            self.pd.close()
+
             if run:
-                os.system("xdg-open '" + where + "/" + path.split("/")[-1] + "' &")
+                os.system("xdg-open '" + where + "/" + items[0].get_absolute_path().split("/")[-1] + "' &")
                 self.api.run_rclone_command(["xdg-open", ], )
 
-        QTimer.singleShot(100, do_download)
+        self.pd.start(do_download)
 
     def item_expanded(self, item):
-        if self.emit_enabled:
-            path = item.get_absolute_path()
-            self.ls(path, item)
+        item.takeChildren()
+        path = item.get_absolute_path()
+        self.ls(path, item)
 
     def item_collapsed(self, item):
-        item.takeChildren()
-        item.addChild(LoadingItem(item))
+        pass
+        # item.takeChildren()
+        # item.addChild(LoadingItem(item))
 
 
 class MainWindow(QMainWindow):
@@ -395,7 +440,7 @@ class MainWindow(QMainWindow):
         self.browser1 = RCloneBrowser()
         #        self.browser2.set_accept_drops(True)
         widget.layout().addWidget(self.browser1)
-        # widget.layout().addWidget(self.browser2)
+        self.setGeometry(100, 100, 600, 400)
 
 
 if __name__ == "__main__":
